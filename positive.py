b421fb298197dc3a78b37a283e14116bdccfee4c
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-02-12 18:03:06 +0530

    positive.py
    check whether a number is positive or zero or negative

---
+++ positive.py
@@ -0,0 +1,16 @@
+n=int(input("enter the number:"))
+
+if(1<=n<100000):
+    
+    print("the given number is positive")
+
+elif(n==0):
+   
+    print("the given number is zero")
+
+elif(n<0):
+    
+    print("the given number is negative")
+else:
+    
+    print("input is wrong")
